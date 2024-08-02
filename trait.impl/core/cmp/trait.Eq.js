a567032679d5c8cc5240b0b40e687d026f012c6f
mciantyre <mciantyre@users.noreply.github.com>
2024-08-02 13:41:40 +0000

    deploy: c7133f2cfe7cae154b93445965f8a544501377dc

--- trait.impl/core/cmp/trait.Eq.js
+++ trait.impl/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"imxrt_ral":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"imxrt_ral/enum.Interrupt.html\" title=\"enum imxrt_ral::Interrupt\">Interrupt</a>"]]
+"imxrt_ral":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"imxrt_ral/enum.Interrupt.html\" title=\"enum imxrt_ral::Interrupt\">Interrupt</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"imxrt_ral":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"imxrt_ral/enum.Interrupt.html\" title=\"enum imxrt_ral::Interrupt\">Interrupt</a>"]]
+"imxrt_ral":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"imxrt_ral/enum.Interrupt.html\" title=\"enum imxrt_ral::Interrupt\">Interrupt</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
