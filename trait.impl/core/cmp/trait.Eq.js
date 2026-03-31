b0a3de49f531eacf60ec28c0253bd1cc0d5417e8
mciantyre <mciantyre@users.noreply.github.com>
2026-03-31 10:53:36 +0000

    deploy: b586d6c8fd976592ed45b9dcbcf9e56ee1f29a9e

--- trait.impl/core/cmp/trait.Eq.js
+++ trait.impl/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["imxrt_ral",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"imxrt_ral/enum.Interrupt.html\" title=\"enum imxrt_ral::Interrupt\">Interrupt</a>",0]]]]);
+    const implementors = Object.fromEntries([["imxrt_ral",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"imxrt_ral/enum.Interrupt.html\" title=\"enum imxrt_ral::Interrupt\">Interrupt</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["imxrt_ral",[["impl&lt;T, const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.0/core/primitive.u8.html\">u8</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"imxrt_ral/struct.Instance.html\" title=\"struct imxrt_ral::Instance\">Instance</a>&lt;T, N&gt;",0]]]]);
+    const implementors = Object.fromEntries([["imxrt_ral",[["impl&lt;T, const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.1/core/primitive.u8.html\">u8</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"imxrt_ral/struct.Instance.html\" title=\"struct imxrt_ral::Instance\">Instance</a>&lt;T, N&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
